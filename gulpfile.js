861ddb0eff6b014e0de1aa75665300093f3ca5c3
Nan Wang <nantas@gmail.com>
2016-04-21 14:40:30 +0800

    add make simulator

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,13 @@ var gulpSequence = require('gulp-sequence');
 var zip = require('gulp-zip');
 var Ftp = require('ftp');
 var ExecSync = require('child_process').execSync;
+var spawn = require('child_process').spawn;
 var Path = require('path');
 
 gulp.task('make-cocos2d-x', gulpSequence('gen-cocos2d-x', 'upload-cocos2d-x'));
+gulp.task('make-platform', gulpSequence('make-prebuilt', 'make-simulator'));
 gulp.task('make-prebuilt', gulpSequence('gen-libs', 'archive-prebuilt', 'upload-prebuilt'));
+gulp.task('make-simulator', gulpSequence('gen-simulator', 'archive-simulator', 'upload-simulator'));
 
 function execSync(cmd, workPath) {
   var execOptions = {
@@ -77,6 +80,78 @@ gulp.task('gen-cocos2d-x', function (cb) {
   cb();
 });
 
+gulp.task('gen-simulator', function (cb) {
+  var cocosConsoleRoot = './tools/cocos2d-console/bin';
+  var cocosConsoleBin;
+  if (process.platform === 'darwin') {
+    cocosConsoleBin = Path.join(cocosConsoleRoot, 'cocos');
+  }
+  else {
+    cocosConsoleBin = Path.join(cocosConsoleRoot, 'cocos.bat');
+  }
+  var args;
+  if (process.platform === 'darwin') {
+    args = ['gen-simulator', '-m', 'debug', '-p', 'mac'];
+  }
+  else {
+    args = ['gen-simulator', '-m', 'debug', '-p', 'win32', '--vs', '2013', '--ol', 'en'];
+  }
+  try {
+    var child = spawn(cocosConsoleBin, args);
+    child.stdout.on('data', function (data) {
+      console.log(data.toString());
+    });
+    child.stderr.on('data', function (data) {
+      console.error(data.toString());
+    });
+    child.on('close', (code) => {
+      if (code !== 0) {
+        console.error('Generate simulator failed');
+        cb();
+        return;
+      }
+
+      //var src, dest;
+
+      //if (process.platform === 'darwin') {
+      //  src = './cocos2d-x/simulator/mac/Simulator.app';
+      //  dest = './utils/simulator/mac/Simulator.app';
+      //}
+      //else {
+      //  src = './cocos2d-x/simulator/win32/';
+      //  dest = './utils/simulator/win32';
+      //}
+      //
+      //if (!Fs.existsSync(src)) {
+      //  console.error(`Can\'t find simulator [${src}]`);
+      //  cb();
+      //  return;
+      //}
+      //
+      //if (Fs.existsSync(dest)) {
+      //  Del.sync(dest, {force: true});
+      //}
+      //
+      //Fs.copySync(src, dest);
+    });
+    child.on('error', function () {
+      console.error('Generate simulator failed');
+      cb();
+    });
+  }
+  catch (err) {
+    console.error(err);
+    cb();
+    return;
+  }
+});
+
+gulp.task('archive-simulator', function () {
+  return gulp.src('./simulator/**/*')
+    .pipe(zip('simulator_' + process.platform + '.zip'))
+    .pipe(gulp.dest('./'));
+});
+
 gulp.task('archive-prebuilt', function () {
   return gulp.src('./prebuilt/**/*', {
     base: './'
@@ -92,4 +167,9 @@ gulp.task('upload-prebuilt', function (cb) {
 gulp.task('upload-cocos2d-x', function (cb) {
   var zipFileName = 'cocos2d-x.zip';
   uploadZipFile(zipFileName, './tools/make-package', cb);
+});
+
+gulp.task('upload-simulator', function (cb) {
+  var zipFileName = 'simulator_' + process.platform + '.zip';
+  uploadZipFile(zipFileName, '.', cb);
 });
