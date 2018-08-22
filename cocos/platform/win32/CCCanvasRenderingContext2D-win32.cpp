384e6a9be528de4840be9f298c0e56fceaf3d8db
leda <li_cheng_zhe@163.com>
2018-08-22 17:58:29 +0800

    replace tab with space

--- cocos/platform/win32/CCCanvasRenderingContext2D-win32.cpp
+++ cocos/platform/win32/CCCanvasRenderingContext2D-win32.cpp
@@ -547,12 +547,12 @@ private:
                 for (int x = 0; x < _bufferWidth; ++x)
                 {
                     COLORREF& clr = *pPixel;
-					uint8_t dirtyValue = GetGValue(clr);
-					// "dirtyValue > 0" means pixel was covered when drawing text
-					if (dirtyValue > 0)
-					{
-						clr = ((BYTE)(dirtyValue * alpha) << 24) | textColor;
-					}
+                    uint8_t dirtyValue = GetGValue(clr);
+                    // "dirtyValue > 0" means pixel was covered when drawing text
+                    if (dirtyValue > 0)
+                    {
+                        clr = ((BYTE)(dirtyValue * alpha) << 24) | textColor;
+                    }
                     ++pPixel;
                 }
             }
