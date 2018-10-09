1a90a90e59df7f41ca1d193b671c681a3a7b6e8a
wen xing liao <liaoxw81@163.com>
2018-10-09 11:03:16 +0800

    [restructure] remove SE_PRECONDITION5

--- cocos/scripting/js-bindings/manual/jsb_conversions.hpp
+++ cocos/scripting/js-bindings/manual/jsb_conversions.hpp
@@ -63,16 +63,6 @@
         } \
     } while(0)
 
-#define SE_PRECONDITION5(condition, ret_value, errorInfo) \
-    do { \
-        if ( ! (condition) ) { \
-            SE_LOGE("jsb: ERROR: File %s: Line: %d, Function: %s\n", __FILE__, __LINE__, __FUNCTION__ ); \
-            se::ScriptEngine* se = se::ScriptEngine::getInstance(); \
-            se->ThrowException(errorInfo); \
-            return (ret_value); \
-        } \
-    } while(0)
-
 #define SE_PRECONDITION4(condition, ret_value, errorCode) \
     do { \
         if ( ! (condition) ) { \
