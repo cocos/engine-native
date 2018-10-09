0014c83a538353db83bbaa8cd00b935baf64cdf3
wen xing liao <liaoxw81@163.com>
2018-10-09 10:47:49 +0800

    fix Differences Between WebGL and OpenGL ES 2.0

--- cocos/scripting/js-bindings/manual/jsb_conversions.hpp
+++ cocos/scripting/js-bindings/manual/jsb_conversions.hpp
@@ -63,6 +63,25 @@
         } \
     } while(0)
 
+#define SE_PRECONDITION4(condition, ret_value, errorInfo) \
+    do { \
+        if ( ! (condition) ) { \
+            SE_LOGE("jsb: ERROR: File %s: Line: %d, Function: %s\n", __FILE__, __LINE__, __FUNCTION__ ); \
+            se::ScriptEngine* se = se::ScriptEngine::getInstance(); \
+            se->ThrowException(errorInfo); \
+            return (ret_value); \
+        } \
+    } while(0)
+
+#define SE_PRECONDITION5(condition, ret_value, errorCode) \
+    do { \
+        if ( ! (condition) ) { \
+            SE_LOGE("jsb: ERROR: File %s: Line: %d, Function: %s\n", __FILE__, __LINE__, __FUNCTION__ ); \
+            __glErrorCode = errorCode; \
+            return (ret_value); \
+        } \
+    } while(0)
+
 #define SE_PRECONDITION_ERROR_BREAK(condition, ...) \
     if ( ! (condition) ) { \
         SE_LOGE("jsb: ERROR: File %s: Line: %d, Function: %s\n", __FILE__, __LINE__, __FUNCTION__ ); \
