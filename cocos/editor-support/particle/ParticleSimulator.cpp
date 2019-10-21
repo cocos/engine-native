865c7804cdec82feec11c8097d6a9d662f58713f
Jno <35944775+Jno1995@users.noreply.github.com>
2019-10-21 21:37:03 +0800

    particle frame update optimize

--- cocos/editor-support/particle/ParticleSimulator.cpp
+++ cocos/editor-support/particle/ParticleSimulator.cpp
@@ -213,6 +213,13 @@ void ParticleSimulator::onDisable()
 
 void ParticleSimulator::render(float dt)
 {
+    if (_lastFrameDt == 0) {
+        _lastFrameDt = dt;
+    }
+    else {
+        dt = fmin(_lastFrameDt, dt);
+    }
+
     if (_finished || _nodeProxy == nullptr || _effect == nullptr)
     {
         return;
