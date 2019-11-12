6d99e313c821ce92d4ab46e9921def246eb535c8
PP <chinese1236@@.com>
2019-11-12 15:07:39 +0800

    update

--- templates/js-template-link/frameworks/runtime-src/Classes/AppDelegate.cpp
+++ templates/js-template-link/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -79,12 +79,14 @@ bool AppDelegate::applicationDidFinishLaunching()
 void AppDelegate::applicationDidEnterBackground()
 {
     EventDispatcher::dispatchEnterBackgroundEvent();
+    // Ensure that handle AudioEngine enter background after all enter background events are handled
     AudioEngine::onEnterBackground();
 }
 
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground()
 {
+    // Ensure that handle AudioEngine enter foreground before all enter foreground events are handled
     AudioEngine::onEnterForeground();
     EventDispatcher::dispatchEnterForegroundEvent();
 }
