5beea62d6b2257b0f2b2174458ae7f0ea0a66c93
minggo <nudtzxm@gmail.com>
2020-01-06 11:23:50 +0800

    add inputassember gpu info

--- cocos/renderer/gfx-metal/MTLGPUObjects.h
+++ cocos/renderer/gfx-metal/MTLGPUObjects.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 #import <Metal/MTLBuffer.h>
 #import <Metal/MTLRenderCommandEncoder.h>
 #import <Metal/MTLRenderPipeline.h>
@@ -29,6 +31,14 @@ struct CCMTLGPUPipelineState
     uint stencilRefBack = 0;
     CCMTLGPUUniformBlockList* vertexUniformBlocks = nullptr;
     CCMTLGPUUniformBlockList* fragmentUniformBlocks = nullptr;
+};
+
+class CCMTLGPUInputAssembler : public Object
+{
+public:
+    id<MTLBuffer> mtlIndexBuffer = nil;
+    id<MTLBuffer> mtlIndirectBuffer = nil;
+    std::vector<id<MTLBuffer>> mtlVertexBufers;
 };
 
 NS_CC_END
--- cocos/renderer/gfx-metal/MTLQueue.mm
+++ cocos/renderer/gfx-metal/MTLQueue.mm
@@ -71,6 +71,7 @@ void CCMTLQueue::executeCommands(const CCMTLCommandPackage* commandPackage)
     CCMTLCmdBeginRenderPass* cmdBeginRenderPass = nullptr;
     CCMTLGPUPipelineState* gpuPipelineState = nullptr;
     CCMTLInputAssembler* inputAssembler = nullptr;
+    CCMTLGPUInputAssembler* gpuInputAssembler = nullptr;
     MTLPrimitiveType primitiveType;
     
     for (uint i = 0; i < commandSize; ++i) {
@@ -131,13 +132,11 @@ void CCMTLQueue::executeCommands(const CCMTLCommandPackage* commandPackage)
                 if (cmd->scissorDirty) [encoder setScissorRect:cmd->scissorRect];
                 
                 // bind vertex buffer
-                //TODO: collect gpub buffers information in CCMTLInputAssembler.
-                // And how to support multiple verte buffers?
                 inputAssembler = cmd->inputAssembler;
                 if (inputAssembler)
                 {
-                    CCMTLBuffer* buffer = static_cast<CCMTLBuffer*>(inputAssembler->vertex_buffers()[0]);
-                    [encoder setVertexBuffer:buffer->getMTLBuffer()
+                    gpuInputAssembler = inputAssembler->getGPUInputAssembler();
+                    [encoder setVertexBuffer:gpuInputAssembler->mtlVertexBufers[0]
                                       offset:0
                                      atIndex:30];
                 }
@@ -149,9 +148,9 @@ void CCMTLQueue::executeCommands(const CCMTLCommandPackage* commandPackage)
                 CCMTLCmdDraw* cmd = commandPackage->drawCmds[cmdIdx++];
                 if (inputAssembler && gpuPipelineState)
                 {
-                    if (!inputAssembler->indirect_buffer() )
+                    if (!gpuInputAssembler->mtlIndirectBuffer)
                     {
-                        if (inputAssembler->index_buffer() && cmd->drawInfo.index_count >= 0)
+                        if (gpuInputAssembler->mtlIndirectBuffer && cmd->drawInfo.index_count >= 0)
                         {
                             uint8_t* offset = 0;
                             offset += cmd->drawInfo.first_index * inputAssembler->index_buffer()->stride();
@@ -161,7 +160,7 @@ void CCMTLQueue::executeCommands(const CCMTLCommandPackage* commandPackage)
                                 [encoder drawIndexedPrimitives:primitiveType
                                                     indexCount:cmd->drawInfo.index_count
                                                      indexType:MTLIndexTypeUInt32
-                                                   indexBuffer:nil
+                                                   indexBuffer:gpuInputAssembler->mtlIndexBuffer
                                              indexBufferOffset:0];
                             }
                             else
