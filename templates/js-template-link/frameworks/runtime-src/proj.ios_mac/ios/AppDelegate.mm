c0b52698f1d90069aa171fe4bea238f190f7f319
minggo <nudtzxm@gmail.com>
2020-03-26 18:02:12 +0800

    fix mistake

--- templates/js-template-link/frameworks/runtime-src/proj.ios_mac/ios/AppDelegate.mm
+++ templates/js-template-link/frameworks/runtime-src/proj.ios_mac/ios/AppDelegate.mm
@@ -1,19 +1,20 @@
 /****************************************************************************
- Copyright (c) 2010-2012 cocos2d-x.org
- Copyright (c) 2013-2017 Chukong Technologies Inc.
-
+ Copyright (c) 2010-2013 cocos2d-x.org
+ Copyright (c) 2013-2016 Chukong Technologies Inc.
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+ 
  http://www.cocos2d-x.org
-
+ 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
-
+ 
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
-
+ 
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,232 +23,88 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
  ****************************************************************************/
-#import "CCApplication.h"
-#import <UIKit/UIKit.h>
-#include "base/CCScheduler.h"
-#include "base/CCAutoreleasePool.h"
-#include "scripting/js-bindings/event/EventDispatcher.h"
-#include "scripting/js-bindings/jswrapper/SeApi.h"
-#include "audio/include/AudioEngine.h"
+#import "AppDelegate.h"
+#import "ViewController.h"
+#import "platform/ios/CCView.h"
 
-@interface MyTimer : NSObject
-{
-    cocos2d::Application* _app;
-    CADisplayLink* _displayLink;
-    int _fps;
+#include "SDKWrapper.h"
+#include "Game.h"
+
+@implementation AppDelegate
+
+Game* game = nullptr;
+@synthesize window;
+
+#pragma mark -
+#pragma mark Application lifecycle
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [[SDKWrapper getInstance] application:application didFinishLaunchingWithOptions:launchOptions];
+    // Add the view controller's view to the window and display.
+    CGRect bounds = [[UIScreen mainScreen] bounds];
+    self.window = [[UIWindow alloc] initWithFrame: bounds];
+
+    // Should create view controller first, cocos2d::Application will use it.
+    _viewController = [[ViewController alloc]init];
+    _viewController.view = [[View alloc] initWithFrame:bounds];
+    _viewController.view.contentScaleFactor = UIScreen.mainScreen.scale;
+    [self.window setRootViewController:_viewController];
+    
+    // cocos2d application instance
+    game = new Game(bounds.size.width, bounds.size.height);
+    game->init();
+    
+    [self.window makeKeyAndVisible];
+    
+    return YES;
 }
-- (instancetype)initWithApp:(cocos2d::Application*)app fps:(int)fps;
-- (void)start;
-- (void)changeFPS:(int)fps;
-- (void)pause;
-- (void)resume;
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    /*
+     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     */
+    [[SDKWrapper getInstance] applicationWillResignActive:application];
+    game->onPause();
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     */
+    [[SDKWrapper getInstance] applicationDidBecomeActive:application];
+    game->onResume();
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    /*
+     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+     If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
+     */
+    [[SDKWrapper getInstance] applicationDidEnterBackground:application];
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    /*
+     Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
+     */
+    [[SDKWrapper getInstance] applicationWillEnterForeground:application];
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    [[SDKWrapper getInstance] applicationWillTerminate:application];
+    delete game;
+    game = nullptr;
+}
+
+#pragma mark -
+#pragma mark Memory management
+
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
+    /*
+     Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
+     */
+}
+
 @end
-
-@implementation MyTimer
-
-- (instancetype)initWithApp:(cocos2d::Application*)app fps:(int)fps
-{
-    if (self = [super init])
-    {
-        _fps = fps;
-        _app = app;
-        _displayLink = [NSClassFromString(@"CADisplayLink") displayLinkWithTarget:self selector:@selector(renderScene:)];
-        _displayLink.preferredFramesPerSecond = _fps;
-    }
-    return self;
-}
-
-- (void)start
-{
-    [_displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-}
-
-- (void)pause
-{
-    [_displayLink invalidate];
-}
-
-- (void)resume
-{
-    [self start];
-}
-
-- (void)changeFPS:(int)fps
-{
-    _displayLink.preferredFramesPerSecond = fps;
-}
-
-- (void)renderScene:(id)sender
-{
-    _app->tick();
-}
-
-@end
-
-NS_CC_BEGIN
-
-namespace
-{
-    bool setCanvasCallback(se::Object* global)
-    {
-        auto viewLogicalSize = cocos2d::Application::getInstance()->getViewLogicalSize();
-        se::ScriptEngine* se = se::ScriptEngine::getInstance();
-        char commandBuf[200] = {0};
-        // https://stackoverflow.com/questions/5795978/string-format-for-intptr-t-and-uintptr-t/41897226#41897226
-        // format intptr_t
-        //set window.innerWidth/innerHeight in css pixel units
-        sprintf(commandBuf, "window.innerWidth = %d; window.innerHeight = %d; window.windowHandler = 0x%" PRIxPTR ";",
-                (int)(viewLogicalSize.x),
-                (int)(viewLogicalSize.y),
-                (uintptr_t)(UIApplication.sharedApplication.delegate.window.rootViewController.view) );
-        se->evalString(commandBuf);
-        return true;
-    }
-    
-    MyTimer* _timer;
-}
-
-Application* Application::_instance = nullptr;
-std::shared_ptr<Scheduler> Application::_scheduler = nullptr;
-
-Application::Application(int width, int height)
-{
-    Application::_instance = this;
-    _scheduler = std::make_shared<Scheduler>();
-    EventDispatcher::init();
-    _viewLogicalSize.x = width;
-    _viewLogicalSize.y = height;
-    
-    _timer = [[MyTimer alloc] initWithApp:this fps:_fps];
-}
-
-Application::~Application()
-{
-#if USE_AUDIO
-    AudioEngine::end();
-#endif
-
-    EventDispatcher::destroy();
-    se::ScriptEngine::destroyInstance();
-
-    Application::_instance = nullptr;
-    
-    [_timer release];
-}
-
-std::string Application::getCurrentLanguageCode() const
-{
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    NSArray *languages = [defaults objectForKey:@"AppleLanguages"];
-    NSString *currentLanguage = [languages objectAtIndex:0];
-    return [currentLanguage UTF8String];
-}
-
-bool Application::isDisplayStats()
-{
-    se::AutoHandleScope hs;
-    se::Value ret;
-    char commandBuf[100] = "cc.debug.isDisplayStats();";
-    se::ScriptEngine::getInstance()->evalString(commandBuf, 100, &ret);
-    return ret.toBoolean();
-}
-
-void Application::setDisplayStats(bool isShow)
-{
-    se::AutoHandleScope hs;
-    char commandBuf[100] = {0};
-    sprintf(commandBuf, "cc.debug.setDisplayStats(%s);", isShow ? "true" : "false");
-    se::ScriptEngine::getInstance()->evalString(commandBuf);
-}
-
-void Application::setCursorEnabled(bool value)
-{
-}
-
-Application::LanguageType Application::getCurrentLanguage() const
-{
-    // get the current language and country config
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    NSArray *languages = [defaults objectForKey:@"AppleLanguages"];
-    NSString *currentLanguage = [languages objectAtIndex:0];
-
-    // get the current language code.(such as English is "en", Chinese is "zh" and so on)
-    NSDictionary* temp = [NSLocale componentsFromLocaleIdentifier:currentLanguage];
-    NSString * languageCode = [temp objectForKey:NSLocaleLanguageCode];
-
-    if ([languageCode isEqualToString:@"zh"]) return LanguageType::CHINESE;
-    if ([languageCode isEqualToString:@"en"]) return LanguageType::ENGLISH;
-    if ([languageCode isEqualToString:@"fr"]) return LanguageType::FRENCH;
-    if ([languageCode isEqualToString:@"it"]) return LanguageType::ITALIAN;
-    if ([languageCode isEqualToString:@"de"]) return LanguageType::GERMAN;
-    if ([languageCode isEqualToString:@"es"]) return LanguageType::SPANISH;
-    if ([languageCode isEqualToString:@"nl"]) return LanguageType::DUTCH;
-    if ([languageCode isEqualToString:@"ru"]) return LanguageType::RUSSIAN;
-    if ([languageCode isEqualToString:@"ko"]) return LanguageType::KOREAN;
-    if ([languageCode isEqualToString:@"ja"]) return LanguageType::JAPANESE;
-    if ([languageCode isEqualToString:@"hu"]) return LanguageType::HUNGARIAN;
-    if ([languageCode isEqualToString:@"pt"]) return LanguageType::PORTUGUESE;
-    if ([languageCode isEqualToString:@"ar"]) return LanguageType::ARABIC;
-    if ([languageCode isEqualToString:@"nb"]) return LanguageType::NORWEGIAN;
-    if ([languageCode isEqualToString:@"pl"]) return LanguageType::POLISH;
-    if ([languageCode isEqualToString:@"tr"]) return LanguageType::TURKISH;
-    if ([languageCode isEqualToString:@"uk"]) return LanguageType::UKRAINIAN;
-    if ([languageCode isEqualToString:@"ro"]) return LanguageType::ROMANIAN;
-    if ([languageCode isEqualToString:@"bg"]) return LanguageType::BULGARIAN;
-    return LanguageType::ENGLISH;
-}
-
-Application::Platform Application::getPlatform() const
-{
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) // idiom for iOS <= 3.2, otherwise: [UIDevice userInterfaceIdiom] is faster.
-        return Platform::IPAD;
-    else
-        return Platform::IPHONE;
-}
-
-bool Application::openURL(const std::string &url)
-{
-    NSString* msg = [NSString stringWithCString:url.c_str() encoding:NSUTF8StringEncoding];
-    NSURL* nsUrl = [NSURL URLWithString:msg];
-    return [[UIApplication sharedApplication] openURL:nsUrl];
-}
-
-void Application::copyTextToClipboard(const std::string &text)
-{
-    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
-    pasteboard.string = [NSString stringWithCString:text.c_str() encoding:NSUTF8StringEncoding];
-}
-
-bool Application::init()
-{
-    se::ScriptEngine* se = se::ScriptEngine::getInstance();
-    se->addRegisterCallback(setCanvasCallback);
-    
-    [_timer start];
-    
-    return true;
-}
-
-void Application::onPause()
-{
-    [_timer pause];
-}
-
-void Application::onResume()
-{
-    [_timer resume];
-}
-
-std::string Application::getSystemVersion()
-{
-    NSString* systemVersion = [UIDevice currentDevice].systemVersion;
-    return [systemVersion UTF8String];
-}
-
-void Application::setPreferredFramesPerSecond(int fps)
-{
-    _fps = fps;
-    [_timer changeFPS:_fps];
-}
-
-NS_CC_END
