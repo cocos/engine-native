dbcdbb8993ac220e0d34566b9264c0ef901b60b8
caryliu <caryliu1999@gmail.com>
2020-04-09 20:27:42 +0800

    refine code

--- cocos/math/Quaternion.cpp
+++ cocos/math/Quaternion.cpp
@@ -155,17 +155,17 @@ Quaternion Quaternion::getInversed() const
 
 void Quaternion::multiply(const Quaternion& q)
 {
-    multiply(*this, q, this);
+    multiply(q, *this, this);
 }
 
 void Quaternion::multiply(const Quaternion& q1, const Quaternion& q2, Quaternion* dst)
 {
     GP_ASSERT(dst);
 
-    float x = q2.x * q1.w + q2.w * q1.x + q2.y * q1.z - q2.z * q1.y;
-    float y = q2.y * q1.w + q2.w * q1.y + q2.z * q1.x - q2.x * q1.z;
-    float z = q2.z * q1.w + q2.w * q1.z + q2.x * q1.y - q2.y * q1.x;
-    float w = q2.w * q1.w - q2.x * q1.x - q2.y * q1.y - q2.z * q1.z;
+    float x = q1.w * q2.x + q1.x * q2.w + q1.y * q2.z - q1.z * q2.y;
+    float y = q1.w * q2.y - q1.x * q2.z + q1.y * q2.w + q1.z * q2.x;
+    float z = q1.w * q2.z + q1.x * q2.y - q1.y * q2.x + q1.z * q2.w;
+    float w = q1.w * q2.w - q1.x * q2.x - q1.y * q2.y - q1.z * q2.z;
 
     dst->x = x;
     dst->y = y;
