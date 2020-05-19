4cf513abd605bbaedbb7f08f86cc3f39b46a1367
PatriceJiang <397136899@qq.com>
2020-05-19 20:38:27 +0800

    use fullpath

--- tools/travis-scripts/run-script.sh
+++ tools/travis-scripts/run-script.sh
@@ -32,7 +32,7 @@ function setup_linux_andorid_sdk()
     echo "Download Android SDK... "
     cmake_dir=$ANDROID_SDK/cmake/3.10.2.4988404/bin
     cd $HOME/bin/android
-    if [ ! -d "$cmake_bin" ]; then
+    if [ ! -d "$cmake_dir" ]; then
         wget -t 5 -q https://dl.google.com/android/repository/commandlinetools-linux-6200805_latest.zip
         unzip *.zip
         yes | ./tools/bin/sdkmanager  --sdk_root="$ANDROID_SDK" \
@@ -67,7 +67,7 @@ function build_android()
 function mac_install_cmake()
 {   
     echo "Compiling CMake ... "
-    if [ ! -d $HOME/bin/cmake/bin ]; then
+    if [ ! -d "$HOME/bin/cmake/bin" ]; then
         cd $HOME/bin
         NUM_OF_CORES=`getconf _NPROCESSORS_ONLN`
         cmake_source=https://github.com/Kitware/CMake/releases/download/v3.17.0/cmake-3.17.0.tar.gz
@@ -88,15 +88,15 @@ function mac_install_cmake()
 function mac_download_cmake()
 {
     echo "Download CMake ..."
-    if [ ! -f cmake_bin.tar.gz ]; then
+    cd $HOME/bin
+    if [ ! -f "$HOME/bin/cmake_bin.tar.gz" ]; then
         cmake_binary=https://github.com/Kitware/CMake/releases/download/v3.17.0/cmake-3.17.0-Darwin-x86_64.tar.gz
         wget -t 3 --no-check-certificate $cmake_binary -O cmake_bin.tar.gz -q
         tar xf cmake_bin.tar.gz 2>/dev/null
     else
         echo " CMake found! Skip downloading!"
     fi
-    cmake_bin_dir=`dirname $(find . -name cmake-gui)`
-    cmake_bin_dir="$PWD/$cmake_bin_dir"
+    cmake_bin_dir=`dirname $(find $HOME/bin -name cmake-gui)`
     export PATH=$cmake_bin_dir:$PATH
 }
 
