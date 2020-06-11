bdae1902b9e3a18efdc2f2da92cf6ddee5b3ca74
minggo <nudtzxm@gmail.com>
2020-06-11 17:39:36 +0800

    add RenderInstancedBuffer

--- cocos/renderer/pipeline/RenderBatchedQueue.h
+++ cocos/renderer/pipeline/RenderBatchedQueue.h
@@ -10,12 +10,12 @@ class CC_DLL RenderBatchedQueue : public cocos2d::Object {
 public:
     RenderBatchedQueue() = default;
     ~RenderBatchedQueue() = default;
-    
+
     void clear();
     void recordCommandBuffer(cocos2d::GFXDevice *, cocos2d::GFXRenderPass *, cocos2d::GFXCommandBuffer *);
-    
+
     CC_INLINE const cocos2d::set<BatchedBuffer *>::type &getQueue() const { return _queue; }
-    
+
 private:
     cocos2d::set<BatchedBuffer *>::type _queue;
 };
