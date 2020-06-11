22ec96777f1cd3f342bc0e8ff8d0497ed44046b2
minggo <nudtzxm@gmail.com>
2020-06-11 16:57:30 +0800

    add RenderView

--- cocos/renderer/pipeline/Define.h
+++ cocos/renderer/pipeline/Define.h
@@ -27,7 +27,21 @@ struct CC_DLL RenderFlowInfo {
     //TODO
 };
 
+struct CC_DLL RenderViewInfo {
+    //TODO
+};
+
+struct CC_DLL RenderTargetInfo {
+    uint width = 0;
+    uint height = 0;
+};
+
 typedef cocos2d::vector<RenderStage *>::type RenderStageList;
 typedef cocos2d::vector<RenderFlow *>::type RenderFlowList;
+
+//TODO
+const uint CAMERA_DEFAULT_MASK = 1;
+//constexpr CAMERA_DEFAULT_MASK = Layers.makeMaskExclude([Layers.BitMask.UI_2D, Layers.BitMask.GIZMOS, Layers.BitMask.EDITOR,
+//                                                           Layers.BitMask.SCENE_GIZMO, Layers.BitMask.PROFILER]);
 
 NS_PP_END
