Mee-gu/cocos2d-x-lite

vk: depth format feature

cocos/renderer/gfx-gles2/GLES2Commands.cc
@@ -68,8 +68,8 @@ GLenum MapGLInternalFormat(GFXFormat format) {
         case GFXFormat::D16S8: return GL_DEPTH_STENCIL_OES;
         case GFXFormat::D24: return GL_DEPTH_COMPONENT;
         case GFXFormat::D24S8: return GL_DEPTH_STENCIL_OES;
-        case GFXFormat::D32F: return GL_DEPTH_COMPONENT;
-        case GFXFormat::D32F_S8: return GL_DEPTH_STENCIL_OES;
+        case GFXFormat::D32F: return GL_DEPTH_COMPONENT32F;
+        case GFXFormat::D32F_S8: return GL_DEPTH32F_STENCIL8;
 
         case GFXFormat::BC1: return GL_COMPRESSED_RGB_S3TC_DXT1_EXT;
         case GFXFormat::BC1_ALPHA: return GL_COMPRESSED_RGBA_S3TC_DXT1_EXT;
@@ -285,10 +285,11 @@ GLenum GFXFormatToGLType(GFXFormat format) {
         case GFXFormat::RGB9E5: return GL_FLOAT;
 
         case GFXFormat::D16: return GL_UNSIGNED_SHORT;
-        case GFXFormat::D16S8: return GL_UNSIGNED_SHORT;
+        case GFXFormat::D16S8: return GL_UNSIGNED_INT_24_8_OES; // no D16S8 support
         case GFXFormat::D24: return GL_UNSIGNED_INT;
         case GFXFormat::D24S8: return GL_UNSIGNED_INT_24_8_OES;
         case GFXFormat::D32F: return GL_FLOAT;
+        case GFXFormat::D32F_S8: return GL_FLOAT_32_UNSIGNED_INT_24_8_REV;
 
         case GFXFormat::BC1: return GL_UNSIGNED_BYTE;
         case GFXFormat::BC1_SRGB: return GL_UNSIGNED_BYTE;

cocos/engine-native

cocos/renderer/gfx-gles2/GLES2Commands.cc
@@ -286,10 +286,11 @@ GLenum GFXFormatToGLType(GFXFormat format) {
         case GFXFormat::RGB9E5: return GL_FLOAT;
 
         case GFXFormat::D16: return GL_UNSIGNED_SHORT;
-        case GFXFormat::D16S8: return GL_UNSIGNED_SHORT;
+        case GFXFormat::D16S8: return GL_UNSIGNED_INT_24_8_OES;
         case GFXFormat::D24: return GL_UNSIGNED_INT;
         case GFXFormat::D24S8: return GL_UNSIGNED_INT_24_8_OES;
-        case GFXFormat::D32F: return GL_FLOAT;
+        case GFXFormat::D32F: return GL_UNSIGNED_INT;
+        case GFXFormat::D32F_S8: return GL_UNSIGNED_INT_24_8_OES;
 
         case GFXFormat::BC1: return GL_UNSIGNED_BYTE;
         case GFXFormat::BC1_SRGB: return GL_UNSIGNED_BYTE;