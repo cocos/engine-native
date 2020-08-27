f971cfe6e204ab6c8be31430dd70b75d6784f7a4
caryliu <caryliu1999@gmail.com>
2020-08-27 11:12:24 +0800

    commit register

--- cocos/scripting/js-bindings/manual/jsb_module_register.cpp
+++ cocos/scripting/js-bindings/manual/jsb_module_register.cpp
@@ -101,6 +101,7 @@ bool jsb_register_all_modules()
     // extension depend on network
     se->addRegisterCallback(register_all_extension);
     se->addRegisterCallback(register_all_dop_bindings);
+    se->addRegisterCallback(register_all_pipeline);
 
 #if (CC_PLATFORM == CC_PLATFORM_MAC_IOS || CC_PLATFORM == CC_PLATFORM_MAC_OSX)
     se->addRegisterCallback(register_javascript_objc_bridge);
