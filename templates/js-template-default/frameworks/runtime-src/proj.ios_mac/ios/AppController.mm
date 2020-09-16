4621006ce9d4f02a94be3bf602ba2d4deea2bd52
PP_Pro <chinese1236@qq.com>
2020-09-16 15:49:10 +0800

    Revert "add note"
    This reverts commit 62e289b78ee7c803ba5a9d8d5a8609d37ec7dcd1.

--- templates/js-template-default/frameworks/runtime-src/proj.ios_mac/ios/AppController.mm
+++ templates/js-template-default/frameworks/runtime-src/proj.ios_mac/ios/AppController.mm
@@ -95,9 +95,6 @@ cocos2d::Device::Rotation _lastRotation;
     cocos2d::Device::Rotation rotation = cocos2d::Device::Rotation::_0;
     UIDevice * device = [UIDevice currentDevice];
     
-    // NOTE: bug on iOS
-    // when the device rotates to LandscapeLeft, device.orientation returns UIDeviceOrientationLandscapeRight
-    // when the device rotates to LandscapeRight, device.orientation returns UIDeviceOrientationLandscapeLeft
     switch(device.orientation)
     {
         case UIDeviceOrientationPortrait:
