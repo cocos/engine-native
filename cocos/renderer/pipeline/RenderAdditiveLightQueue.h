7facc72843f189c69e1f995399e18c043ca13575
troublemaker52025 <troublemaker52025@outlook.com>
2020-11-19 16:49:38 +0800

    update

--- cocos/renderer/pipeline/RenderAdditiveLightQueue.h
+++ cocos/renderer/pipeline/RenderAdditiveLightQueue.h
@@ -18,6 +18,7 @@ class Device;
 struct Sphere;
 class Shader;
 class ForwardPipeline;
+class DescriptorSet;
 
 struct AdditiveLightPass {
     const SubModelView *subModel = nullptr;
@@ -36,10 +37,13 @@ public:
     void gatherLightPasses(const RenderView *view, gfx::CommandBuffer *cmdBuffer);
 
 private:
+    void clear();
     void updateUBOs(const RenderView *view, gfx::CommandBuffer *cmdBuffer);
+    void updateLightDescriptorSet(const RenderView *view, gfx::CommandBuffer *cmdBuffer);
+    void updateGlobalDescriptorSet(const RenderView *view, gfx::CommandBuffer *cmdBuffer);
     bool getLightPassIndex(const ModelView *model, vector<uint> &lightPassIndices) const;
     bool cullingLight(const Light *light, const ModelView *model);
-    void updateShadowsUBO(gfx::DescriptorSet *, const Light *, gfx::CommandBuffer *cmdBufferer) const;
+    gfx::DescriptorSet *getOrCreateDescriptorSet(const Light *);
 
 private:
     ForwardPipeline *_pipeline = nullptr;
@@ -54,8 +58,12 @@ private:
     RenderInstancedQueue *_instancedQueue = nullptr;
     RenderBatchedQueue *_batchedQueue = nullptr;
     gfx::Buffer *_lightBuffer = nullptr;
-    gfx::Buffer *_firstlightBufferView = nullptr;
+    gfx::Buffer *_firstLightBufferView = nullptr;
     gfx::Sampler *_sampler = nullptr;
+
+    std::map<const Light *, gfx::DescriptorSet *> _descriptorSetMap;
+    std::array<float, UBOGlobal::COUNT> _globalUBO;
+    std::array<float, UBOShadow::COUNT> _shadowUBO;
 
     float _fpScale = 0;
     bool _isHDR = false;
