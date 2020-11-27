5392b1375f1f3e4ef964f133deac66b0c2558616
Arnold <40414978+PatriceJiang@users.noreply.github.com>
2020-11-27 18:24:37 +0800

    fix path

--- tools/travis-scripts/generate-pr.sh
+++ tools/travis-scripts/generate-pr.sh
@@ -16,7 +16,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_ROOT="$DIR/../.."
-TOJS_ROOT="$PROJECT_ROOT/tools/tojs"
+TRAVIS_ROOT="$PROJECT_ROOT/tools/travis-scripts"
 JS_AUTO_GENERATED_DIR="$PROJECT_ROOT/cocos/bindings/auto"
 CCFILES_AUTO_GENERATED_DIR="$PROJECT_ROOT/templates/cocos2dx_files.json"
 COMMITTAG="[ci skip][AUTO]: updating jsbinding automatically"
