7d3e11063759a01f5cb8f6b4fdb6894536b367a0
caryliu <caryliu1999@gmail.com>
2020-12-03 14:05:13 +0800

    fix ci

--- cocos/platform/win32/CanvasRenderingContext2D-win32.cpp
+++ cocos/platform/win32/CanvasRenderingContext2D-win32.cpp
@@ -517,7 +517,7 @@ private:
                     // Because text is drawn in white color, and background color is black,
                     // so the red value is equal to alpha value. And we should keep this value
                     // as it includes anti-atlas information.
-                    uint8_t alpha = GetRValue(clr) ？ 255 : 0;
+                    uint8_t alpha = GetRValue(clr) ? 255 : 0;
                     if (alpha > 0) {
                         val = (alpha << 24) | textColor;
                     }
