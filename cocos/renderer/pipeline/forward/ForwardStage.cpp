d44a6ddeec6bdeea4892c508f9bff28fe1ce6efe
caryliu <caryliu1999@gmail.com>
2020-12-22 16:48:15 +0800

    add delete of ui phase

--- cocos/renderer/pipeline/forward/ForwardStage.cpp
+++ cocos/renderer/pipeline/forward/ForwardStage.cpp
@@ -87,6 +87,7 @@ void ForwardStage::destroy() {
     CC_SAFE_DELETE(_instancedQueue);
     CC_SAFE_DELETE(_additiveLightQueue);
     CC_SAFE_DELETE(_planarShadowQueue);
+    CC_SAFE_DELETE(_uiPhase);
     RenderStage::destroy();
 }
 
