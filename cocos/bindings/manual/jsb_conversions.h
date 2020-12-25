49bec4efaed1049bd0743103e6324af0214aa42b
江战 <40414978+PatriceJiang@users.noreply.github.com>
2020-12-25 18:31:45 +0800

    Update cocos/bindings/manual/jsb_conversions.h

--- cocos/bindings/manual/jsb_conversions.h
+++ cocos/bindings/manual/jsb_conversions.h
@@ -118,7 +118,7 @@ struct InputAssemblerInfo;
         break;                                                                                      \
     }
 
-#if 0
+#if CC_ENABLE_CACHE_JSB_FUNC_RESULT 
 #define SE_HOLD_RETURN_VALUE(retCXXValue, thisObject, jsValue) \
     if CC_CONSTEXPR (is_jsb_object_v<typename std::decay<decltype(retCXXValue)>::type> && (jsValue).isObject()) {\
         (thisObject)->setProperty("__cache" __FUNCTION__, (jsValue)); \
