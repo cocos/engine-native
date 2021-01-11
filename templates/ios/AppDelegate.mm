82d16f441fca3250593be745953befcb6840f209
xin.yang <953459485@qq.com>
2021-01-11 13:26:16 +0800

    merge Cocos Service changed!

--- templates/ios/AppDelegate.mm
+++ templates/ios/AppDelegate.mm
@@ -39,7 +39,7 @@ Game* game = nullptr;
 #pragma mark Application lifecycle
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-     [[SDKWrapper shared] application:application didFinishLaunchingWithOptions:launchOptions];
+    [[SDKWrapper shared] application:application didFinishLaunchingWithOptions:launchOptions];
     // Add the view controller's view to the window and display.
     CGRect bounds = [[UIScreen mainScreen] bounds];
     self.window = [[UIWindow alloc] initWithFrame: bounds];
@@ -65,7 +65,7 @@ Game* game = nullptr;
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
      */
-     [[SDKWrapper shared] applicationWillResignActive:application];
+    [[SDKWrapper shared] applicationWillResignActive:application];
     game->onPause();
 }
 
@@ -82,18 +82,18 @@ Game* game = nullptr;
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
      If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
      */
-     [[SDKWrapper shared] applicationDidEnterBackground:application];
+    [[SDKWrapper shared] applicationDidEnterBackground:application];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     /*
      Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
      */
-     [[SDKWrapper shared] applicationWillEnterForeground:application];
+    [[SDKWrapper shared] applicationWillEnterForeground:application];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-     [[SDKWrapper shared] applicationWillTerminate:application];
+    [[SDKWrapper shared] applicationWillTerminate:application];
     delete game;
     game = nullptr;
 }
