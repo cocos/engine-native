1c8f45ba0147918aa9aa8315fcc6885793a738f5
timlyeee <timelyeee@gmail.com>
2021-06-25 10:34:05 +0800

    Adapte common control dll link for win64 and win32

--- tools/simulator/frameworks/runtime-src/proj.win32/SimulatorWin.cpp
+++ tools/simulator/frameworks/runtime-src/proj.win32/SimulatorWin.cpp
@@ -25,8 +25,14 @@
 
 
 #pragma comment(lib, "comctl32.lib")
-#pragma comment(linker, "\"/manifestdependency:type='Win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='X86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#ifdef UNICODE
+    #if defined _M_IX86
+        #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+    #elif defined _M_X64
+        #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
+    #endif
+#endif
 #include "cocos/base/UTF8.h"
 #include "stdafx.h"
 #include <io.h>
