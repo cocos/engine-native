16456f09d00ecd5661ba0ef9b1a5793859155d93
ref-ptr <ref-ptr@qq.com>
2021-07-14 11:34:14 +0800

    check framebuffer cache before binding framebuffer

--- cocos/renderer/gfx-gles2/GLES2Commands.cpp
+++ cocos/renderer/gfx-gles2/GLES2Commands.cpp
@@ -2610,11 +2610,14 @@ void cmdFuncGLES2CopyBuffersToTexture(GLES2Device *device, const uint8_t *const 
 }
 
 CC_GLES2_API void cmdFuncGLES2CopyTextureToBuffers(GLES2Device *device, GLES2GPUTexture *gpuTexture, uint8_t *const *buffers, const BufferTextureCopy *regions, uint count) {
-    GLuint  framebuffer = 0;
+    GLuint framebuffer = device->framebufferCacheMap()->getFramebufferFromTexture(gpuTexture);
     auto    glFormat    = mapGLFormat(gpuTexture->format);
     auto    glType      = formatToGLType(gpuTexture->format);
-    framebuffer = device->framebufferCacheMap()->getFramebufferFromTexture(gpuTexture);
-    GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, framebuffer));
+
+    if (device->stateCache()->glFramebuffer != framebuffer) {
+        GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, framebuffer));
+        device->stateCache()->glFramebuffer = framebuffer;
+    }
     for(uint32_t i = 0; i < count; ++i) {
         auto region = regions[i];
         auto w          = region.texExtent.width;
@@ -2623,7 +2626,6 @@ CC_GLES2_API void cmdFuncGLES2CopyTextureToBuffers(GLES2Device *device, GLES2GPU
         uint8_t* copyDst = buffers[i];
         GL_CHECK(glReadPixels(region.texOffset.x, region.texOffset.y, region.texExtent.width, region.texExtent.height, glFormat, glType, copyDst));
     }
-    GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, 0));
 }
 
 void cmdFuncGLES2BlitTexture(GLES2Device *device, GLES2GPUTexture *gpuTextureSrc, GLES2GPUTexture *gpuTextureDst, const TextureBlit *regions, uint count, Filter filter) {
