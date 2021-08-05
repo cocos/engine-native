15cddcc4b53709f2e9a3f0dd8399b3f6f708bf71
lijilin <jilin.li@cocos.com>
2021-08-05 19:31:22 +0800

    refactor for memory leak detector

--- cocos/base/memory/MemoryHook.cpp
+++ cocos/base/memory/MemoryHook.cpp
@@ -26,38 +26,29 @@
 #include "MemoryHook.h"
 #if USE_MEMORY_LEAK_DETECTOR
 
-#include "base/Log.h"
 #include <sstream>
-
 
 #if CC_PLATFORM == CC_PLATFORM_ANDROID
 #define __GNU_SOURCE
 #include <dlfcn.h>
 
-#define likely_if(x)   if (__builtin_expect((x), 1))
-#define unlikely_if(x) if (__builtin_expect((x), 0))
-
-typedef void* (*MallocType)(size_t size);
-typedef void  (*FreeType)(void* ptr);
-
-typedef void (*NewHookType)(const void* ptr, size_t size);
-typedef void (*DeleteHookType)(const void* ptr);
-
-NewHookType g_new_hooker = nullptr;
-DeleteHookType g_delete_hooker = nullptr;
+static NewHookType g_new_hooker = nullptr;
+static DeleteHookType g_delete_hooker = nullptr;
 
 extern "C" {
-void* malloc(size_t __size) __attribute__ ((weak));
+
+void* malloc(size_t size) __attribute__ ((weak));
 void free(void* ptr) __attribute__ ((weak));
 
+// Use strong symbol to overwrite the weak one.
 void* malloc(size_t size) {
     static MallocType system_malloc = nullptr;
-    unlikely_if(system_malloc == nullptr) {
+    UNLIKELY_IF(system_malloc == nullptr) {
         system_malloc = (MallocType)dlsym(RTLD_NEXT, "malloc");
     }
 
     void* ptr = system_malloc(size);
-    likely_if(g_new_hooker != nullptr) {
+    LIKELY_IF(g_new_hooker != nullptr) {
         g_new_hooker(ptr, size);
     }
 
@@ -66,12 +57,12 @@ void* malloc(size_t size) {
 
 void free(void* ptr) {
     static FreeType system_free = nullptr;
-    unlikely_if(system_free == nullptr) {
+    UNLIKELY_IF(system_free == nullptr) {
         system_free = (FreeType)dlsym(RTLD_NEXT, "free");
     }
 
     system_free(ptr);
-    likely_if(g_delete_hooker != nullptr) {
+    LIKELY_IF(g_delete_hooker != nullptr) {
         g_delete_hooker(ptr);
     }
 }
@@ -79,34 +70,46 @@ void free(void* ptr) {
 }
 
 #elif CC_PLATFORM == CC_PLATFORM_MAC_IOS || CC_PLATFORM == CC_PLATFORM_MAC_OSX
-typedef void (*NewHookType)(const void* ptr, size_t size);
-typedef void (*DeleteHookType)(const void* ptr);
-
-typedef void
-malloc_logger_t(uint32_t aType,
+typedef void malloc_logger_t(uint32_t aType,
                 uintptr_t aArg1, uintptr_t aArg2, uintptr_t aArg3,
                 uintptr_t aResult, uint32_t aNumHotFramesToSkip);
+
 extern malloc_logger_t* malloc_logger;
-static malloc_logger_t* g_default_malloc_logger;
-NewHookType g_new_hooker = nullptr;
-DeleteHookType g_delete_hooker = nullptr;
+static malloc_logger_t* g_system_malloc_logger = nullptr;
+static NewHookType      g_new_hooker    = nullptr;
+static DeleteHookType   g_delete_hooker = nullptr;
 
 static void
-cocos_malloc_logger(uint32_t aType,
+cc_malloc_logger(uint32_t aType,
                  uintptr_t aArg1, uintptr_t aArg2, uintptr_t aArg3,
                  uintptr_t aResult, uint32_t aNumHotFramesToSkip)
 {
-    // malloc/calloc/valloc/realloc
     if (aResult != 0) {
-        if (g_new_hooker) {
-            const void* ptr = reinterpret_cast<const void*>(aResult);
-            size_t size = reinterpret_cast<size_t>(aArg2);
-            g_new_hooker(ptr, size);
+        size_t new_size = reinterpret_cast<size_t>(aArg3);
+        if (new_size != 0) {
+            // realloc
+            LIKELY_IF(g_delete_hooker != nullptr) {
+                const void* ptr = reinterpret_cast<const void*>(aArg2);
+                g_delete_hooker(ptr);
+            }
+
+            LIKELY_IF(g_new_hooker != nullptr) {
+                const void* new_ptr  = reinterpret_cast<const void*>(aResult);
+                g_new_hooker(ptr, new_size);
+            }
+        }
+        else {
+            // malloc/calloc/valloc
+            LIKELY_IF(g_new_hooker != nullptr) {
+                const void* ptr  = reinterpret_cast<const void*>(aResult);
+                size_t      size = reinterpret_cast<size_t>(aArg2);
+                g_new_hooker(ptr, size);
+            }
         }
     }
-    // free
     else {
-        if (g_delete_hooker) {
+        // free
+        LIKELY_IF(g_delete_hooker != nullptr) {
             const void* ptr = reinterpret_cast<const void*>(aArg2);
             g_delete_hooker(ptr);
         }
@@ -114,6 +117,7 @@ cocos_malloc_logger(uint32_t aType,
 }
 
 #elif CC_PLATFORM == CC_PLATFORM_WINDOWS
+#include <Windows.h>
 
 extern "C" {
 typedef void (*MallocHook_NewHook)(const void* ptr, size_t size);
@@ -131,27 +135,12 @@ namespace cc {
 
 static void newHook(const void* ptr, size_t size) {
     uint64_t address = reinterpret_cast<uint64_t>(ptr);
-    MemoryHook::getInstance()->addRecord(address, size);
+    GMemoryHook.addRecord(address, size);
 }
 
 static void deleteHook(const void* ptr) {
     uint64_t address = reinterpret_cast<uint64_t>(ptr);
-    MemoryHook::getInstance()->removeRecord(address);
-}
-
-MemoryHook* MemoryHook::_instance = nullptr;
-
-MemoryHook* MemoryHook::getInstance() {
-    if (_instance == nullptr) {
-        _instance = new MemoryHook();        
-    }
-
-    return _instance;
-}
-
-void MemoryHook::destroyInstance() {
-    delete _instance;
-    _instance = nullptr;
+    GMemoryHook.removeRecord(address);
 }
 
 MemoryHook::MemoryHook() {
@@ -210,46 +199,59 @@ void MemoryHook::dumpMemoryLeak() {
     startStream << "---------------------------------------------------------------------------------------------------------" << std::endl;
     startStream << "--------------------------------------memory leak report start-------------------------------------------" << std::endl;
     startStream << "---------------------------------------------------------------------------------------------------------" << std::endl;
-    CC_LOG_INFO("%s", startStream.str().c_str());
+    log(startStream.str());
 
     if (_records.size() == 0) {
         std::stringstream stream;
         stream << std::endl;
         stream << "Congratulations! There is no memory leak at all." << std::endl;
-        CC_LOG_INFO("%s", stream.str().c_str());
+        log(stream.str());
     }
 
     int i = 0;
+    size_t total = 0;
     for (const auto& iter : _records) {
         std::stringstream stream;
+        int k = 0;
+        total += iter.second.size;
+
         stream << std::endl;
-        stream << "<" << ++i << ">:" << "leak " << iter.second.size 
-            << " bytes at 0x" << std::hex << iter.second.address << std::dec << std::endl;
+        stream << "<" << ++i << ">:" << "leak " << iter.second.size << " bytes at 0x" << std::hex << iter.second.address << std::dec << std::endl;
         stream << "\tcallstack:" << std::endl;
         auto frames = CallStack::backtraceSymbols(iter.second.callstack);
-        int  k      = 0;
         for (auto& frame : frames) {
             stream << "\t[" << ++k << "]:" << frame.toString() << std::endl;
         }
 
-        CC_LOG_INFO("%s", stream.str().c_str());
+        log(stream.str());
     }
 
     std::stringstream endStream;
-    endStream << std::endl;
+    endStream << std::endl << "Total Leak: " << total << " bytes" << std::endl;
     endStream << "---------------------------------------------------------------------------------------------------------" << std::endl;
     endStream << "--------------------------------------memory leak report end---------------------------------------------" << std::endl;
     endStream << "---------------------------------------------------------------------------------------------------------" << std::endl;
-    CC_LOG_INFO("%s", endStream.str().c_str());
+    log(endStream.str());
+}
+
+void MemoryHook::log(const std::string& msg) {
+#if (CC_PLATFORM == CC_PLATFORM_ANDROID)
+    __android_log_write(ANDROID_LOG_WARN, "Cocos", msg.c_str());
+#elif CC_PLATFORM == CC_PLATFORM_MAC_IOS || CC_PLATFORM == CC_PLATFORM_MAC_OSX
+    fputs(msg.c_str(), stdout);
+#elif (CC_PLATFORM == CC_PLATFORM_WINDOWS)
+    OutputDebugStringA(msg.c_str());
+#endif
 }
 
 void MemoryHook::registerAll() {
-#if CC_PLATFORM == CC_PLATFORM_ANDROID
+    #if CC_PLATFORM == CC_PLATFORM_ANDROID
     g_new_hooker = newHook;
     g_delete_hooker = deleteHook;
-#elif CC_PLATFORM == CC_PLATFORM_MAC_IOS || CC_PLATFORM == CC_PLATFORM_MAC_OSX
-    g_default_malloc_logger = malloc_logger;
-    malloc_logger = cocos_malloc_logger;
+    free(malloc(1));    // force to init system_malloc/system_free
+    #elif CC_PLATFORM == CC_PLATFORM_MAC_IOS || CC_PLATFORM == CC_PLATFORM_MAC_OSX
+    g_system_malloc_logger = malloc_logger;
+    malloc_logger = cc_malloc_logger;
     g_new_hooker = newHook;
     g_delete_hooker = deleteHook;
 #elif CC_PLATFORM_WINDOWS
@@ -263,7 +265,7 @@ void MemoryHook::unRegisterAll() {
     g_new_hooker = nullptr;
     g_delete_hooker = nullptr;
 #elif CC_PLATFORM == CC_PLATFORM_MAC_IOS || CC_PLATFORM == CC_PLATFORM_MAC_OSX
-    malloc_logger = g_default_malloc_logger;
+    malloc_logger   = g_system_malloc_logger;
     g_new_hooker = nullptr;
     g_delete_hooker = nullptr;
 #elif CC_PLATFORM_WINDOWS
