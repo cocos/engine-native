3ef2a8e9490076f7cb17db5f758ad44ce6e9d50a
timlyeee <timelyeee@gmail.com>
2021-09-15 11:41:46 +0800

    Clang tidy 2

--- cocos/renderer/gfx-gles3/GLES3GPUContext.cpp
+++ cocos/renderer/gfx-gles3/GLES3GPUContext.cpp
@@ -331,7 +331,7 @@ bool GLES3GPUContext::makeCurrent(EGLSurface drawSurface, EGLSurface readSurface
     return succeeded;
 }
 
-void GLES3GPUContext::resetStates() const // NOLINT(readability-function-size)
+void GLES3GPUContext::resetStates() const // NOLINT(google-readability-function-size)
 {
     GL_CHECK(glPixelStorei(GL_PACK_ALIGNMENT, 1));
     GL_CHECK(glPixelStorei(GL_UNPACK_ALIGNMENT, 1));
