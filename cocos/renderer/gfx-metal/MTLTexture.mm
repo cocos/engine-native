b75df185b26ca2f250e563df79ae68e330d82615
Zeqiang Li <Zeqiang-li@outlook.com>
2021-09-17 18:27:24 +0800

    temp memoryless strategy

--- cocos/renderer/gfx-metal/MTLTexture.mm
+++ cocos/renderer/gfx-metal/MTLTexture.mm
@@ -175,7 +175,7 @@ bool CCMTLTexture::createMTLTexture() {
     descriptor.mipmapLevelCount = _info.levelCount;
     descriptor.arrayLength = _info.type == TextureType::CUBE ? 1 : _info.layerCount;
 
-    if(hasAllFlags(TextureUsage::COLOR_ATTACHMENT | TextureUsage::DEPTH_STENCIL_ATTACHMENT | TextureUsage::INPUT_ATTACHMENT, _info.usage) && mu::isImageBlockSupported()) {
+    if(hasAllFlags(TextureUsage::COLOR_ATTACHMENT | TextureUsage::INPUT_ATTACHMENT, _info.usage) && mu::isImageBlockSupported()) {
         //xcode OS version warning
         if (@available(macOS 11.0, *)) {
             descriptor.storageMode = MTLStorageModeMemoryless;
