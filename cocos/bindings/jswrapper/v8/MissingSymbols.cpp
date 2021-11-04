d4c1aa6888e62fcf611380b4a5e670e327113f6b
timlyeee <timelyeee@gmail.com>
2021-11-04 10:28:40 +0800

    move ohos symbol to cpp

--- cocos/bindings/jswrapper/v8/MissingSymbols.cpp
+++ cocos/bindings/jswrapper/v8/MissingSymbols.cpp
@@ -22,25 +22,17 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
 ****************************************************************************/
-#include <cstddef>
-#include <cerrno>
-#include <cstring>
-
 #if SCRIPT_ENGINE_TYPE == SCRIPT_ENGINE_V8
-#if CC_PLATFORM == CC_PLATFORM_ANDROID
-#if __ANDROID_API__ < 21
-extern "C" {
-int localSigemptyset(void *set) {
-    if (set == NULL) { // NOLINT(modernize-use-nullptr)
-        errno = EINVAL;
-        return -1;
-    }
-    memset(set, 0, sizeof(unsigned long)); // NOLINT(google-runtime-int)
-    return 0;
-}
 
-int sigemptyset(void *set) __attribute__((weak, alias("localSigemptyset")));
+//TODO(PatriceJiang): modify this when OHOS llvm upgrade
+#if CC_PLATFORM == CC_PLATFORM_OHOS
+extern "C" {
+int local_bcmp(const void *cs, const void *ct, size_t count) {
+return memcmp(cs, ct, count);
 }
+int bcmp(const void *cs, const void *ct, size_t count) __attribute__((weak, alias("local_bcmp")));
+} // extern "C"
 #endif
-#endif
+
+
 #endif // #if SCRIPT_ENGINE_TYPE == SCRIPT_ENGINE_V8
