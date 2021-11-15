ec44af83cfb11f5ba54c79fdc52ea61c32b2fa0e
timlyeee <timelyeee@gmail.com>
2021-11-15 11:43:45 +0800

    Using self defined function to replace strdup

--- cocos/network/HttpClient-java.cpp
+++ cocos/network/HttpClient-java.cpp
@@ -549,7 +549,10 @@ private:
             return nullptr;
         }
         std::string strValue = cc::StringUtils::getStringUTFCharsJNI(env, jstr);
-        return strdup(strValue.c_str());
+        size_t size = strValue.size()+1;
+        char* retVal = static_cast<char *>(malloc(size));
+        memcpy(retVal, strValue.c_str(), size);
+        return retVal;
     }
 
     int getCStrFromJByteArray(jbyteArray jba, JNIEnv *env, char **ppData) { //NOLINT(readability-convert-member-functions-to-static)
