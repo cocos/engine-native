6f8a08fdd72dcc11a02df8cf77dbec0ea9ac47ec
Greg1129 <zuiaikuailong@gmail.com>
2021-12-23 19:55:22 +0800

    fix issue 3233 2D blend

--- cocos/renderer/scene/assembler/SimpleSprite2D.cpp
+++ cocos/renderer/scene/assembler/SimpleSprite2D.cpp
@@ -93,6 +93,26 @@ void SimpleSprite2D::fillBuffers(NodeProxy* node, ModelBatcher* batcher, std::si
     float* dstWorldVerts = buffer->vData + vBufferOffset;
     memcpy(dstWorldVerts, data->getVertices(), 4 * _bytesPerVertex);
 
+    uint8_t* ptrAlpha = (uint8_t*)data->getVertices() + _alphaOffset;
+    size_t dataPerVertex = _bytesPerVertex / sizeof(uint8_t);
+    uint8_t* ptrColor = (uint8_t*)dstWorldVerts + _vfColor->offset;
+
+    for (uint32_t i = 0; i < 4; ++i)
+    {
+        float alpha = *(ptrAlpha) / 255.0;
+
+        uint8_t valueB = *(ptrAlpha - 1) * alpha;
+        uint8_t valueG = *(ptrAlpha - 2) * alpha;
+        uint8_t valueR = *(ptrAlpha - 3) * alpha;
+
+        *ptrColor       = valueR;
+        *(ptrColor + 1) = valueG;
+        *(ptrColor + 2) = valueB;
+
+        ptrAlpha += dataPerVertex;
+        ptrColor += dataPerVertex;
+    }
+
     // Copy index buffer with vertex offset
     uint16_t* srcIndices = (uint16_t*)data->getIndices();
     uint16_t* dstIndices = buffer->iData;
@@ -100,6 +120,37 @@ void SimpleSprite2D::fillBuffers(NodeProxy* node, ModelBatcher* batcher, std::si
     {
         dstIndices[indexId++] = vertexId + srcIndices[j];
     }
+}
+
+void SimpleSprite2D::updateOpacity(std::size_t index, uint8_t opacity)
+{
+    // has no color info in vertex buffer
+    if(!_vfColor || !_datas || !_vfmt)
+    {
+        return;
+    }
+    
+    const IARenderData& ia = _iaDatas[index];
+    std::size_t meshIndex = ia.meshIndex >= 0 ? ia.meshIndex : index;
+    
+    RenderData* data = _datas->getRenderData(meshIndex);
+    if (!data)
+    {
+        return;
+    }
+    
+    CCASSERT(data->getVBytes() % _bytesPerVertex == 0, "Assembler::updateOpacity vertices data doesn't follow vertex format");
+    uint32_t vertexCount = (uint32_t)data->getVBytes() / _bytesPerVertex;
+    
+    size_t dataPerVertex = _bytesPerVertex / sizeof(uint8_t);
+    uint8_t* ptrAlpha = (uint8_t*)data->getVertices() + _alphaOffset;
+    for (uint32_t i = 0; i < vertexCount; ++i)
+    {
+       *ptrAlpha = opacity;
+       ptrAlpha += dataPerVertex;
+    }
+    
+    *_dirty &= ~VERTICES_OPACITY_CHANGED;
 }
 
 RENDERER_END
