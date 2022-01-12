478111007d0e8d7095640b978287697c4f8eee91
timlyeee <timelyeee@gmail.com>
2022-01-12 10:42:50 +0800

    vec3

--- cocos/platform/java/jni/JniHelper.h
+++ cocos/platform/java/jni/JniHelper.h
@@ -313,6 +313,7 @@ public:
 #ifndef __OHOS__
             ccDeleteLocalRef(t.env, t.classID);
 #endif
+            ccDeleteLocalRef(t.env, array);
             deleteLocalRefs(t.env, &localRefs);
         } else {
             reportError(className, methodName, signature);
